26a1b7da8ac355363e9f71e9a9d8054cc946bfac
geloro94 <geloro94@gmail.com>
2021-04-15 15:51:47 +0200

    Added Json Schema and Validation for all generated json.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from MapperDataModel import generate_map
-from UiDataModel import TerminologyEntry, prune_terminology_tree
-from geccoToUI import create_terminology_definition_for, get_categories, IGNORE_CATEGORIES, MAIN_CATEGORIES
-from termEntryToExcel import to_excel
-from queryTermCodeMapper import to_term_code_node
-import os, errno
-from os import path
+import errno
+import json
+import os
 import shutil
+from os import path
+from jsonschema import validate
+
+from MapperDataModel import generate_map
+from UiDataModel import TerminologyEntry, prune_terminology_tree, TermCode
+from geccoToUI import create_terminology_definition_for, get_categories, IGNORE_CATEGORIES, MAIN_CATEGORIES
+from queryTermCodeMapper import to_term_code_node
+from termEntryToExcel import to_excel
 
 GECCO_VERSION = "de.gecco 1.0.3"
 MII_LAB_VERSION = "de.medizininformatikinitiative.kerndatensatz.laborbefund 1.0.2"
@@ -39,15 +43,22 @@ def download_gecco_profile():
 
 if __name__ == '__main__':
     download_gecco_profile()
-    others = TerminologyEntry(None, "CategoryEntry", selectable=False)
+    othersTermCode = TermCode("num.codex", "Andere", "Andere")
+    others = TerminologyEntry(othersTermCode, "CategoryEntry", selectable=False)
     others.display = "Andere"
     category_entries = create_terminology_definition_for(get_categories())
     map_entries = generate_map(category_entries)
     map_entries_file = open("mapping/" + "TermCodeMapping.json", 'w')
     map_entries_file.write(map_entries.to_json())
+    map_entries_file.close()
+    map_entries_file = open("mapping/" + "TermCodeMapping.json", 'r')
+    validate(instance=json.load(map_entries_file), schema=json.load(open("schema/term-code-mapping-schema.json")))
     term_code_tree = to_term_code_node(category_entries)
     term_code_file = open("mapping/" + "TermCodeTree.json", 'w')
     term_code_file.write(term_code_tree.to_json())
+    term_code_file.close()
+    term_code_file = open("mapping/" + "TermCodeTree.json", 'r')
+    validate(instance=json.load(term_code_file), schema=json.load(open("schema/codex-code-tree-schema.json")))
     for category in category_entries:
         prune_terminology_tree(category, 2)
     to_excel(category_entries)
@@ -57,7 +68,13 @@ if __name__ == '__main__':
         if category.display in MAIN_CATEGORIES:
             f = open("result/" + category.display.replace("/ ", "") + ".json", 'w')
             f.write(category.to_json())
+            f.close()
+            f = open("result/" + category.display.replace("/ ", "") + ".json", 'r')
+            validate(instance=json.load(f), schema=json.load(open("schema/ui-profile-schema.json")))
         else:
             others.children.append(category)
     f = open("result/" + others.display + ".json", 'w')
     f.write(others.to_json())
+    f.close()
+    f = open("result/" + others.display + ".json", 'r')
+    validate(instance=json.load(f), schema=json.load(open("schema/ui-profile-schema.json")))
