8bc29b20d12473894b600b449b681e5be9453934
Michael Folz <folz@med.uni-frankfurt.de>
2024-07-15 15:59:18 +0200

    #18 - Merging of multiple ontologies
    - add scripts to create databases and functions
    - add sqlmerger class that imports all correctly prefixed dumps in a folder into different schemas and then copies them all to one db and dumps this db as a result

---
+++ test/util/SqlMerger_test.py
@@ -0,0 +1,27 @@
+import unittest
+import os
+from util.SqlMerger import SqlMerger
+
+
+class SqlMergerTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.sql_merger = SqlMerger(
+            sql_init_script_dir='../../resources/sql-scripts',
+            sql_script_dir='./scripts'
+        )
+        cls.merged_script_targetfile = os.path.abspath(f'{cls.sql_merger.sql_script_dir}/R__Load_latest_ui_profile.sql')
+
+    @classmethod
+    def tearDownClass(cls):
+        if os.path.exists(cls.merged_script_targetfile):
+            os.remove(cls.merged_script_targetfile)
+
+    def test_merge(self):
+        self.sql_merger.execute_merge()
+        self.assertTrue(os.path.exists(self.merged_script_targetfile))
+
+
+if __name__ == '__main__':
+    unittest.main()
