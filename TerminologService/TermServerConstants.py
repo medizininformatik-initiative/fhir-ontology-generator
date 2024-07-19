572c816cf3d6eb648311f86741e513ad377eea05
shah1375 <shayanmohajerani@gmail.com>
2024-07-19 10:55:31 +0200

    get the current working directory and from there on set the path of default mapping file

--- TerminologService/TermServerConstants.py
+++ TerminologService/TermServerConstants.py
@@ -4,7 +4,11 @@ import os
 TERMINOLOGY_SERVER_ADDRESS = os.environ.get('ONTOLOGY_SERVER_ADDRESS')
 SERVER_CERTIFICATE = os.environ.get('SERVER_CERTIFICATE')
 PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
-mapping_path = os.environ.get('ONTO_MAPPING_PATH', "../mapping.json")
+
+# Get the current working directory
+current_working_dir = os.getcwd()
+print(current_working_dir)
+mapping_path = os.environ.get('ONTO_MAPPING_PATH', os.path.join(current_working_dir, '..', '..', 'mapping.json'))
 with open(mapping_path, 'r') as file:
     MAPPING_ONTO_VERSION = json.load(file)
 
