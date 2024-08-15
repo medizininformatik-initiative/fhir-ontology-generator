0511035d6194b4198f862e7393604333b015f912
Michael Folz <folz@med.uni-frankfurt.de>
2024-08-15 15:43:50 +0200

    #29 - Add Elastic Search ontology generation
    - use argparse and allow to set input/output file as well as additional files to pack
    - add elastic search index generation files for codeable_concept and ontology

--- core/ElasticSearchBulkGenerator.py
+++ core/ElasticSearchBulkGenerator.py
@@ -1,6 +1,7 @@
 import os
 import json
 import uuid
+import zipfile
 from zipfile import ZipFile
 
 
@@ -14,27 +15,9 @@ class ElasticSearchGenerator:
         pass
 
     @staticmethod
-    def __get_contextualized_termcode_hash(context_node, termcode_node, namespace_uuid_str):
-        context_system = context_node['system']
-        context_code = context_node['code']
-        if 'version' in context_node:
-            context_version = context_node['version']
-        else:
-            context_version = ''
+    def __get_contextualized_termcode_hash(context_node: dict, termcode_node: dict, namespace_uuid_str):
 
-        termcode_system = termcode_node['system']
-        termcode_code = termcode_node['code']
-        if 'version' in termcode_node:
-            termcode_version = termcode_node['version']
-        else:
-            termcode_version = ''
-
-        context_termcode_hash_input = context_system + \
-            context_code + \
-            context_version + \
-            termcode_system + \
-            termcode_code + \
-            termcode_version
+        context_termcode_hash_input = f"{context_node.get('system')}{context_node.get('code')}{context_node.get('version', '')}{termcode_node.get('system')}{termcode_node.get('code')}{termcode_node.get('version', '')}"
 
         namespace_uuid = uuid.UUID(namespace_uuid_str)
         return str(uuid.uuid3(namespace_uuid, context_termcode_hash_input))
@@ -43,60 +26,58 @@ class ElasticSearchGenerator:
     def __iterate_tree(node, target, parent, namespace_uuid_str):
         # For now, we agreed upon just using the first termcode if there are more than one
         term_code = node['termCodes'][0]
-        obj = {}
-        obj['hash'] = ElasticSearchGenerator.__get_contextualized_termcode_hash(node['context'], term_code,
-                                                                                namespace_uuid_str=namespace_uuid_str)
-        obj['name'] = term_code['display']
-        obj['availability'] = 0
-        obj['terminology'] = term_code['system']
-        obj['termcode'] = term_code['code']
+        obj = {'hash': ElasticSearchGenerator.__get_contextualized_termcode_hash(node['context'], term_code,
+                                                                                 namespace_uuid_str=namespace_uuid_str),
+               'name': term_code['display'],
+               'availability': 0,
+               'terminology': term_code['system'],
+               'termcode': term_code['code'],
+               'selectable': node['selectable'],
+               'context': node['context'],
+               'termcodes': node['termCodes'],
+               'criteria_sets': [],
+               'translations': [],
+               'parents': [],
+               'children': [],
+               'related_terms': []
+        }
         # Not present in ui_tree files
         # obj['kdsModule'] = term_code['module']
-        obj['selectable'] = node['selectable']
-        obj['context'] = node['context']
-        obj['termcodes'] = node['termCodes']
         # not yet available in ontology generator...
-        obj['criteria_sets'] = []
-        obj['translations'] = []
-        obj['parents'] = []
-        obj['children'] = []
-        obj['related_terms'] = []
         if parent:
             # For now, we agreed upon just using the first termcode if there are more than one
             parent_term_code = parent['termCodes'][0]
-            parent_obj = {}
-            parent_obj['name'] = parent_term_code['display']
-            parent_obj['contextualized_termcode_hash']\
-                = ElasticSearchGenerator.__get_contextualized_termcode_hash(parent['context'], parent_term_code,
-                                                                            namespace_uuid_str=namespace_uuid_str)
+            parent_obj = {'name': parent_term_code['display'],
+                          'contextualized_termcode_hash': ElasticSearchGenerator.__get_contextualized_termcode_hash(
+                              parent['context'], parent_term_code,
+                              namespace_uuid_str=namespace_uuid_str)}
             obj['parents'].append(parent_obj)
         if len(node['termCodes']) > 1:
             for relatedTerm in node['termCodes']:
                 if relatedTerm == term_code:
                     continue
-                related_terms_obj = {}
-                related_terms_obj['name'] = relatedTerm['display']
+                related_terms_obj = {'name': relatedTerm['display'],
+                                     'contextualized_termcode_hash': ElasticSearchGenerator.__get_contextualized_termcode_hash(
+                                         node['context'], relatedTerm,
+                                         namespace_uuid_str=namespace_uuid_str)}
                 # Currently, the contextualizedtermcodehash is on node-lvl. Related terms will always have the same id.
                 # THIS IS A PROBLEM!
-                related_terms_obj['contextualized_termcode_hash'] \
-                    = ElasticSearchGenerator.__get_contextualized_termcode_hash(node['context'], relatedTerm,
-                                                                                namespace_uuid_str=namespace_uuid_str)
                 obj['related_terms'].append(related_terms_obj)
 
         target.append(obj)
         if 'children' in node:
             for child in node['children']:
                 for childTermCode in child['termCodes']:
-                    child_obj = {}
-                    child_obj['name'] = childTermCode['display']
-                    child_obj['contextualized_termcode_hash'] \
-                        = ElasticSearchGenerator.__get_contextualized_termcode_hash(
-                        child['context'], childTermCode, namespace_uuid_str=namespace_uuid_str)
+                    child_obj = {'name': childTermCode['display'],
+                                 'contextualized_termcode_hash': ElasticSearchGenerator.__get_contextualized_termcode_hash(
+                                     child['context'], childTermCode, namespace_uuid_str=namespace_uuid_str)}
                     obj['children'].append(child_obj)
                 ElasticSearchGenerator.__iterate_tree(child, target, node, namespace_uuid_str=namespace_uuid_str)
 
     @staticmethod
     def generate_elasticsearch_files(input_file,
+                                     output_file,
+                                     include_additional_files,
                                      work_dir='.',
                                      namespace_uuid_str='00000000-0000-0000-0000-000000000000',
                                      index_name='ontology',
@@ -145,11 +126,17 @@ class ElasticSearchGenerator:
                                 current_file.close()
                                 current_file = open(current_file_name, 'w')
 
-        with ZipFile(input_file, 'a') as backend_zip:
+        with ZipFile(output_file, 'w', zipfile.ZIP_DEFLATED) as elastic_zip:
             # Add the new files to the zipfile
             for root, dirs, files in os.walk(work_dir):
                 for file in files:
                     if file.startswith(filename_prefix) and file.endswith(extension):
                         os.chdir(root)
-                        backend_zip.write(filename=file, arcname=f"elastic/{file}")
+                        elastic_zip.write(filename=file, arcname=f"{file}")
                         os.remove(file)
+            # Add files from additional folders if any
+            if include_additional_files:
+                for root, dirs, files in os.walk(include_additional_files):
+                    for file in files:
+                        file_path = os.path.join(root, file)
+                        elastic_zip.write(file_path, os.path.relpath(file_path, include_additional_files))
--- example/generate_elasticsearch_files.py
+++ example/generate_elasticsearch_files.py
@@ -1,4 +1,10 @@
+import argparse
 from core.ElasticSearchBulkGenerator import ElasticSearchGenerator
 
 if __name__ == '__main__':
-    ElasticSearchGenerator.generate_elasticsearch_files(input_file='mii_core_data_set/ontology/backend.zip')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--input-file', dest='input_file', type=str, default='ontology/backend.zip', action='store')
+    parser.add_argument('-w', '--output-file', dest='output_file', type=str, default='ontology/elastic.zip', action='store')
+    parser.add_argument('-i', '--additional-files-directory', dest='additional_files_directory', type=str, default='resources/elastic', action='store')
+    args = parser.parse_args()
+    ElasticSearchGenerator.generate_elasticsearch_files(input_file=args.input_file, output_file=args.output_file, include_additional_files=args.additional_files_directory)
