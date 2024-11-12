12dfede847ef5d896227edd4f7edaf28f195d43f
Frontman50 <leonard.csomai@gmail.com>
2024-11-13 00:43:52 +0100

    refactoring, bug fixing and implementing feedback on last pull request

---
+++ example/code_systems_translations/Readme.md
@@ -0,0 +1,1 @@
+folder to store code-system translations
--- generate_elasticsearch_files.py
+++ generate_elasticsearch_files.py
@@ -4,7 +4,12 @@ from core.ElasticSearchBulkGenerator import ElasticSearchGenerator
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--ontology_dir', type=str)
-    parser.add_argument( '--availability_input_dir', type=str)
+    parser.add_argument('--availability_input_dir', type=str)
     parser.add_argument('--generate_availability', action='store_true')
+    parser.add_argument('--code_system_translations_folder', type=str, default='example/code_systems_translations')
     args = parser.parse_args()
-    ElasticSearchGenerator.generate_elasticsearch_files(ontology_dir=args.ontology_dir, generate_availability=args.generate_availability, availability_input_dir=args.availability_input_dir)
+    ElasticSearchGenerator.generate_elasticsearch_files(ontology_dir=args.ontology_dir,
+                                                        generate_availability=args.generate_availability,
+                                                        availability_input_dir=args.availability_input_dir,
+                                                        code_system_translations_folder=args.code_system_translations_folder
+                                                        )
