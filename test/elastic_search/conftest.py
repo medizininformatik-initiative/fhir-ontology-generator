19a6402a36ab595479901c8b31a1ff877955e162
paul.behrend <paul.behrend@uksh.de>
2025-01-24 19:11:15 +0100

    Test:   - Refactor elastic search tests   - Improve setup and teardown
    Util:
      - Add OAuth to client for feasibility backend

--- test/elastic_search/conftest.py
+++ test/elastic_search/conftest.py
@@ -1,19 +1,74 @@
+import os
+import subprocess
+import zipfile
+
 import pytest
 import requests
+import logging
 
-from helper_functions import get_and_upload_test_data_to_fhir
+
+logger = logging.getLogger(__name__)
+
+
+def save_docker_logs():
+    """
+    Test to save logs of all running Docker containers to a logs folder.
+    This should be the last test in the file to ensure all other tests are complete.
+    """
+    # Directory of the (this) test file
+    test_dir_path = os.path.dirname(os.path.realpath(__file__))
+    output_folder = os.path.join(test_dir_path, "docker_logs")
+    os.makedirs(output_folder, exist_ok=True)
+
+    try:
+        # Get the list of running container IDs
+        result = subprocess.run(
+            ["docker", "ps", "-q"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True
+        )
+        container_ids = result.stdout.strip().split("\n")
+
+        if not container_ids or container_ids == ['']:
+            logger.info("No running containers found.")
+            return
+
+        # Save logs for each container
+        for container_id in container_ids:
+            log_file = os.path.join(output_folder, f"{container_id}_logs.txt")
+            with open(log_file, "w") as f:
+                subprocess.run(
+                    ["docker", "logs", container_id],
+                    stdout=f,
+                    stderr=subprocess.PIPE,
+                    text=True,
+                    check=True
+                )
+            logger.info(f"Logs saved for container {container_id} in {log_file}")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error while fetching logs: {e.stderr}")
+
 
 
 @pytest.fixture(scope="session")
-def docker_compose_file():
-    return "docker-compose.yml"
+def test_dir() -> str:
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+@pytest.fixture(scope="session")
+def docker_compose_file(test_dir: str) -> str:
+    yield os.path.join(test_dir, "docker-compose.yml")
+    save_docker_logs()
+
 
 def is_responsive(url) -> bool:
     try:
         response = requests.get(url, timeout=5)
         if response.status_code == 200:
             return True
-        print(f"Got Status-Code: {response.status_code} from url: {url}")
+        logger.info(f"Got Status-Code: {response.status_code} from url: {url}")
     except requests.RequestException:
         pass
     return False
@@ -25,7 +80,7 @@ def backend_ip(docker_services):
     url = f"http://127.0.0.1:{port}"
     url_health_test=url+"/actuator/health"
 
-    print(f"Waiting for service '{dataportal_backend_name}' to become responsive at {url_health_test}...")
+    logger.info(f"Waiting for service '{dataportal_backend_name}' to become responsive at {url_health_test}...")
     docker_services.wait_until_responsive(
         timeout=180.0,
         pause=0.1,
@@ -33,22 +88,58 @@ def backend_ip(docker_services):
     )
     return url
 
+
+def get_and_upload_test_data(fhir_url, test_dir_path: str,
+                             repo_url="https://github.com/medizininformatik-initiative/mii-testdata/releases/download/v1.0.1/kds-testdata-2024.0.1.zip"):
+    """
+    Helper function which downloads the testdata from the specified repository, unpacks it and uploads it to the specified fhir_url server
+    :param fhir_url: url of fhir which to upload the test data to
+
+    :param repo_url: url of repository which to download the testdata from
+    """
+    response = requests.get(repo_url, timeout=5)
+    zip_path = os.path.join("kds-testdata-2024.0.1.zip")
+    with open(zip_path, 'wb') as file:
+        file.write(response.content)
+    test_data_dir_path = os.path.join(test_dir_path, "testdata")
+    os.makedirs(test_data_dir_path, exist_ok=True)
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(test_data_dir_path)
+    logger.info("Downloaded testdata successfully")
+
+    # will be replaced so it uses middleware in backend
+    resource_dir = os.path.join(test_data_dir_path,"kds-testdata-2024.0.1","resources")
+    for file_name in os.listdir(resource_dir):
+        if file_name.endswith(".json"):
+            with open(os.path.join(resource_dir, file_name), "r", encoding="utf-8") as f:
+                data = f.read()
+            response = requests.post(
+                f"{fhir_url}",
+                headers={"Content-Type": "application/fhir+json"},
+                data=data.encode("utf-8"),
+                timeout=10)
+            logger.info(f"Uploaded {file_name} with status code:  {response.status_code}")
+    return True
+
+
 @pytest.fixture(scope="session")
-def fhir_ip(docker_services):
+def fhir_ip(docker_services, test_dir):
     fhir_name = "blaze"
     port = docker_services.port_for(fhir_name, 8080)
     url = f"http://127.0.0.1:{port}"
     url_health_test = url+"/fhir/metadata"
 
-    print(f"Waiting for service '{fhir_name}' to become responsive at {url_health_test}...")
+    logger.info(f"Waiting for service '{fhir_name}' to become responsive at {url_health_test}...")
     docker_services.wait_until_responsive(
         timeout=90.0,
-        pause=5,
+        pause=0.1,
         check=lambda: is_responsive(url_health_test)
     )
+
     # upload testdata for fhir server for testing
-    get_and_upload_test_data_to_fhir(url)
+    get_and_upload_test_data(url, test_dir)
     return url
+
 
 @pytest.fixture(scope="session")
 def elastic_ip(docker_services):
@@ -56,11 +147,10 @@ def elastic_ip(docker_services):
     port = docker_services.port_for(elastic_name, 9200)
     url = f"http://127.0.0.1:{port}"
 
-    print(f"Waiting for service '{elastic_name}' to be responsive at {url}...")
+    logger.info(f"Waiting for service '{elastic_name}' to be responsive at {url}...")
     docker_services.wait_until_responsive(
         timeout=90.0,
         pause=0.1,
         check=lambda: is_responsive(url)
     )
     return url
-
