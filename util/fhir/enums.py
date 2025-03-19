c6e97827889cb89c458ea4ee397c0c2a49d8208d
paul.behrend <paul.behrend@uksh.de>
2025-03-19 15:39:06 +0100

    Add new model classes for DSE
    Profile Detail Generation:
      - Add model classes

--- util/fhir/enums.py
+++ util/fhir/enums.py
@@ -3,7 +3,7 @@ from enum import unique, Enum, EnumMeta
 from typing_extensions import Self
 
 
-class PrimitiveFhirTypeMeta(EnumMeta):
+class FhirPrimitiveDataTypeMeta(EnumMeta):
     def __contains__(cls, item: str | Self) -> bool:
         try:
             cls(item)
@@ -13,7 +13,7 @@ class PrimitiveFhirTypeMeta(EnumMeta):
 
 
 @unique
-class PrimitiveFhirType(Enum, metaclass=PrimitiveFhirTypeMeta):
+class FhirPrimitiveDataType(Enum, metaclass=FhirPrimitiveDataTypeMeta):
     INSTANT = 'instant'
     TIME = 'time'
     DATE = 'date'
@@ -33,3 +33,78 @@ class PrimitiveFhirType(Enum, metaclass=PrimitiveFhirTypeMeta):
     UUID = 'uuid'
     UNSIGNED_INT = 'unsignedInt'
     POSITIVE_INT = 'positiveInt'
+
+
+class FhirComplexDataTypeEnumMeta(EnumMeta):
+    def __contains__(cls, item: str | Self) -> bool:
+        try:
+            cls(item)
+        except ValueError:
+            return False
+        return True
+
+
+@unique
+class FhirComplexDataType(Enum, metaclass=FhirComplexDataTypeEnumMeta):
+    ELEMENT = "Element"
+    RATIO = "Ratio"
+    PERIOD = "Period"
+    RANGE = "Range"
+    ATTACHMENT = "Attachment"
+    IDENTIFIER = "Identifier"
+    ANNOTATION = "Annotation"
+    HUMAN_NAME = "HumanName"
+    CODEABLE_CONCEPT = "CodeableConcept"
+    CONTACT_POINT = "ContactPoint"
+    CODING = "Coding"
+    MONEY = "Money"
+    ADDRESS = "Address"
+    BACKBONE_ELEMENT = "BackboneElement"
+    TIMING = "Timing"
+    DOSAGE = "Dosage"
+    ELEMENT_DEFINITION = "ElementDefinition"
+    QUANTITY = "Quantity"
+    AGE = "Age"
+    DISTANCE = "Distance"
+    DURATION = "Duration"
+    COUNT = "Count"
+    MONEY_QUANTITY = "MoneyQuantity"
+    SIMPLE_QUANTITY = "SimpleQuantity"
+    SAMPLED_DATA = "SampledData"
+    SIGNATURE = "Signature"
+    CONTRIBUTOR = "Contributor"
+    DATA_REQUIREMENT = "DateRequirement"
+    RELATED_ARTIFACT = "RelatedArtifact"
+    USAGE_CONTEXT = "UsageContext"
+    PARAMETER_DEFINITION = "ParameterDefinition"
+    EXPRESSION = "Expression"
+    TRIGGER_DEFINITION = "TriggerDefinition"
+    REFERENCE = "Reference"
+    META = "Meta"
+    XHTML = "xhtml"
+    NARRATIVE = "Narrative"
+    EXTENSION = "Extension"
+
+
+FhirDataType = FhirPrimitiveDataType | FhirComplexDataType
+
+
+class FhirSearchTypeMeta(EnumMeta):
+    def __contains__(cls, item: str | Self) -> bool:
+        try:
+            cls(item)
+        except ValueError:
+            return False
+        return True
+
+
+@unique
+class FhirSearchType(str, Enum, metaclass=FhirSearchTypeMeta):
+    NUMBER = "number"
+    DATE = "date"
+    STRING = "string"
+    TOKEN = "token"
+    REFERENCE = "reference"
+    COMPOSITE = "composite"
+    QUANTITY = "quantity"
+    URI = "uri"
