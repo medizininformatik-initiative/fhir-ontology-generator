959640cdbd2b329d1e5ef38724742a9ae6bff296
Frontman50 <leonard.csomai@gmail.com>
2025-06-16 13:39:40 +0300

    Add more json schemata for output files
    mappings, elastic, fhir_search

--- tests/integration/integrity/json/conftest.py
+++ tests/integration/integrity/json/conftest.py
@@ -1,8 +1,14 @@
 import json
 import os
 from pathlib import Path
+from urllib import request
 
 import pytest
+from _pytest.python import Metafunc
+
+from common.util.project import Project
+from ...conftest import project
+
 
 def __test_dir() -> Path:
     return Path(os.path.dirname(os.path.realpath(__file__))).resolve()
@@ -11,12 +17,9 @@ def __test_dir() -> Path:
 def test_dir() -> Path:
     return __test_dir()
 
-@pytest.fixture(scope="session")
-def schema_store(test_dir) -> dict:
+def _schema_store(test_dir) -> dict:
     store = {}
     base_path = os.path.join(test_dir,"schemata","common")
-    # print(base_path)
-
     print(os.listdir(base_path))
 
     for schema in os.listdir(base_path):
@@ -26,5 +29,36 @@ def schema_store(test_dir) -> dict:
                 schema = json.load(f)
                 if "$id" in schema:
                     store[schema["$id"]] = schema
-
     return store
+
+@pytest.fixture(scope="session")
+def schema_store(test_dir) -> dict:
+    return _schema_store(test_dir)
+
+
+def pytest_generate_tests(metafunc: Metafunc):
+    """
+    Generates tests dynamically based on the collected querying metadata files within the project directory
+    """
+    if "test_json_integrity_elastic_file" == metafunc.definition.name:
+        config = metafunc.config
+        # project = project
+        test_dir = __test_dir()
+
+        project_name = "fdpg-ontology"
+        if project_name is None:
+            raise ValueError("Command line option '--project' has to provided with a proper project name as its value")
+        project = Project(name=project_name)
+
+
+        elastic_folder = project.output / "merged_ontology" / "elastic"
+        list_of_id_to_be_tested = os.listdir(elastic_folder)
+        list_to_be_tested = [elastic_folder / elastic_file for elastic_file in os.listdir(elastic_folder)]
+
+
+        metafunc.parametrize(
+            argnames=("json_file", "test_dir", "schema_store"),
+            argvalues=[(instance, test_dir, _schema_store(test_dir)) for instance in list_to_be_tested],
+            ids=[it for it in list_of_id_to_be_tested],
+            scope="session"
+        )
--- tests/integration/integrity/json/json_integrity_test.py
+++ tests/integration/integrity/json/json_integrity_test.py
@@ -8,13 +8,14 @@ from json_source_map import calculate
 from os.path import basename
 from pathlib import Path
 
-from networkx.algorithms.bipartite.projection import project
-
 from common.util.log.functions import get_logger
 
 from jsonschema import RefResolver
 
 logger = get_logger(__file__)
+
+
+
 
 def validate_json_with_file(file: Path, schema: Path, schema_store: dict):
     logger.info(f"Validating {file} : with :{schema}")
@@ -25,32 +26,38 @@ def validate_json_with_file(file: Path, schema: Path, schema_store: dict):
     with open(schema) as f:
         json_schema = json.load(f)
 
-    #registry = Registry(retrieve_from_filesystem)
+    return validate_json_object(json_file, json_schema, schema_store, file, schema)
 
+def validate_json_object(json_content: dict, json_schema: dict, schema_store: dict, json_file: Path|bool = 0, schema_file: Path = 0):
     resolver = RefResolver.from_schema(json_schema, store=schema_store)
 
     try:
-        jsonschema.validate(instance=json_file, schema=json_schema, resolver=resolver)
-    except jsonschema.ValidationError as exc:
-        pointer = "/" + "/".join(str(p) for p in exc.absolute_path)
-        print(pointer_map.get(pointer))
-        loc = pointer_map[pointer].to_dict().get("value")
-        line = loc.get("line", 0) + 1  # json-source-map uses 0-based index
-        column = loc.get("column", 0) + 1
+        jsonschema.validate(instance=json_content, schema=json_schema, resolver=resolver)
+    except (jsonschema.ValidationError, jsonschema.exceptions.ValidationError) as exc :
 
-        clickable = f"{file}:{line}:{column}"  # clickable in many IDEs/terminals
-        print(clickable)
+        # delete original json content from memory for performance
+        del json_content
 
-        clickable_pycharm = f'File "{clickable}", line {line}, in {exc.path}: mmm'
+        filename_with_coords = "ProvidedJsonObj"
 
-        logger.error(f"Validation for {basename(file)} failed because:  \n{exc.message} \n"
+        # loading pointer_map for detailed output on console in case of an error
+        if json_file != 0:
+            with open(json_file) as f:
+                raw = f.read()
+                pointer_map = calculate(raw)
+            pointer = "/" + "/".join(str(p) for p in exc.absolute_path)
+            loc = pointer_map[pointer].to_dict().get("value")
+            line = loc.get("line", 0) + 1  # uses index form 0
+            column = loc.get("column", 0) + 1
+            filename_with_coords = f"{json_file}:{line}:{column}"  # clickable in many IDEs/terminals - id does not work in PyCharm
+
+        logger.error(f"Validation for {basename(json_file)} failed because:  \n{exc.message} \n"
                      f"Path to error: {list(exc.path)} \n"
                      f"Absolute path to error: {list(exc.absolute_path)} \n"
-                     f"{clickable_pycharm}")
-        # logger.error(f"Error: {exc}")
+                     f"{filename_with_coords}")
         return False
-    except jsonschema.SchemaError as exc:
-        logger.error(f"Validation for {basename(schema)} failed because:  \n"
+    except (jsonschema.SchemaError, jsonschema.exceptions.SchemaError) as exc:
+        logger.error(f"Validation for {basename(schema_file)} failed because:  \n"
                      f"Path to error: {list(exc.path)} \n"
                      f"Absolute path to error: {list(exc.absolute_path)} \n"
                      f"{exc.message} \n")
@@ -58,9 +65,9 @@ def validate_json_with_file(file: Path, schema: Path, schema_store: dict):
         return True
 
 def test_json_integrity_profile_tree(test_dir, project, schema_store):
+    # fails because there are some missing translations
     profile_tree_file = project.output /  "merged_ontology" / "profile_tree.json"
-    profile_tree_schema_file = Path(os.path.join(test_dir,"schemata", "profile_tree_schema.json"))
-
+    profile_tree_schema_file = Path(os.path.join(test_dir, "schemata", "profile_tree_schema.json"))
     assert True == validate_json_with_file(file=profile_tree_file, schema=profile_tree_schema_file, schema_store=schema_store)
 
 def test_json_integrity_term_code_info(test_dir, project, schema_store):
@@ -78,3 +85,61 @@ def test_json_integrity_ui_trees(test_dir, project, schema_store):
     for ui_tree_file in os.listdir(ui_tree_folder):
         ui_tree_file = ui_tree_folder / ui_tree_file
         assert True == validate_json_with_file(file=ui_tree_file, schema=ui_tree_schema_file, schema_store=schema_store)
+
+def test_json_integrity_mapping_cql(test_dir, project, schema_store):
+    mapping_cql = project.output /  "merged_ontology" / "mapping" / "cql" / "mapping_cql.json"
+    mapping_cql_schema_file = Path(os.path.join(test_dir, "schemata", "mapping_cql_schema.json"))
+    assert True == validate_json_with_file(file=mapping_cql, schema=mapping_cql_schema_file, schema_store=schema_store)
+
+def test_json_integrity_mapping_fhir(test_dir, project, schema_store):
+    mapping_fhir = project.output /  "merged_ontology" / "mapping" / "fhir" / "mapping_fhir.json"
+    mapping_fhir_schema_file = Path(os.path.join(test_dir, "schemata", "mapping_fhir_schema.json"))
+    assert True == validate_json_with_file(file=mapping_fhir, schema=mapping_fhir_schema_file, schema_store=schema_store)
+
+def test_json_integrity_mapping_tree(test_dir, project, schema_store):
+    mapping_tree = project.output / "merged_ontology" / "mapping" / "mapping_tree.json"
+    mapping_tree_schema_file = Path(os.path.join(test_dir, "schemata", "mapping_tree_schema.json"))
+    assert True == validate_json_with_file(file=mapping_tree, schema=mapping_tree_schema_file,schema_store=schema_store)
+
+def test_json_integrity_mapping_tree_dse(test_dir, project, schema_store):
+    mapping_tree_dse = project.output / "merged_ontology" / "mapping" / "dse_mapping_tree.json"
+    mapping_tree_dse_schema_file = Path(os.path.join(test_dir, "schemata", "mapping_tree_dse_schema.json"))
+    assert True == validate_json_with_file(file=mapping_tree_dse, schema=mapping_tree_dse_schema_file, schema_store=schema_store)
+
+def test_json_integrity_elastic_file(json_file, test_dir, schema_store):
+
+    elastic_ontology_index_schema_file = Path(os.path.join(test_dir, "schemata", "elastic_ontology_index_schema.json"))
+    with open(elastic_ontology_index_schema_file) as f:
+        elastic_ontology_index_schema_file = json.load(f)
+
+    elastic_ontology_content_schema_file = Path(
+        os.path.join(test_dir, "schemata", "elastic_ontology_content_schema.json"))
+    with open(elastic_ontology_content_schema_file) as f:
+        elastic_ontology_content_schema_file = json.load(f)
+
+    logger.info(f"Currently testing: {json_file}")
+    line_number = 0
+    with open(json_file) as f:
+        while True:
+            elastic_ontology_index_line = f.readline()
+            if not elastic_ontology_index_line: break # eof
+
+            elastic_ontology_content_line = f.readline()
+            if not elastic_ontology_content_line: break # eof
+
+            elastic_ontology_index_line = json.loads(elastic_ontology_index_line)
+            elastic_ontology_content_line = json.loads(elastic_ontology_content_line)
+
+            line_number += 2
+
+            try:
+                assert True == validate_json_object(json_content=elastic_ontology_index_line,
+                                                    json_schema=elastic_ontology_index_schema_file,
+                                                    schema_store=schema_store)
+                assert True == validate_json_object(json_content=elastic_ontology_content_line,
+                                                    json_schema=elastic_ontology_content_schema_file,
+                                                    schema_store=schema_store)
+            except Exception as e:
+                logger.error(f"LineNumber: {line_number}")
+
+
