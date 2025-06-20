ed1510b116cdda81c0e6bff5240f9535b9ebcbd7
Frontman50 <leonard.csomai@gmail.com>
2025-06-20 19:54:04 +0200

    changed method to more robust approach than relying on node name

--- data_selection_extraction/core/generators/profile_tree.py
+++ data_selection_extraction/core/generators/profile_tree.py
@@ -417,14 +417,14 @@ class ProfileTreeGenerator:
                 return SnapshotPackageScope.DEFAULT
 
     @classmethod
-    def __condense_profile_tree(cls, profile_tree: ProfileTreeNode) -> ProfileTreeNode:
+    def __condense_profile_tree(cls, profile_tree: ProfileTreeNode, distance_from_root: int = 0) -> ProfileTreeNode:
         """
         Condenses a given profile tree by removing intermediate nodes that are not selectable and do not have multiple
         children
         :param profile_tree: `ProfileTreeNode` instance representing the root of a profile tree to condense
         :return: Condensed profile tree
         """
-        if len(profile_tree.children) > 1 or profile_tree.selectable or profile_tree.leaf or "modul" in profile_tree.name:
+        if len(profile_tree.children) > 1 or profile_tree.selectable or profile_tree.leaf or distance_from_root == 1:
             tree = profile_tree.model_copy()
         else:
             # Exactly one child element should exist at this point since the node has neither more than one child nor is
@@ -433,5 +433,5 @@ class ProfileTreeGenerator:
             cls.__logger.info(f"Removing node [id='{profile_tree.id}', name='{profile_tree.name}'] from tree "
                               f"[children={child_names}]")
             tree = profile_tree.children[0].model_copy()
-        tree.children = [ProfileTreeGenerator.__condense_profile_tree(n) for n in tree.children]
+        tree.children = [ProfileTreeGenerator.__condense_profile_tree(n, distance_from_root+1) for n in tree.children]
         return tree
