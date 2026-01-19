b32c4a7bdc1aee57cfa4e165e2ae095b94eef5bf
Frontman50 <leonard.csomai@gmail.com>
2026-01-19 15:26:34 +0100

    Saving prototype for generating lookup tables

---
+++ flattening/scripts/generate_lookup.py
@@ -0,0 +1,22 @@
+from common.util.log.functions import get_logger
+from common.util.project import Project
+from flattening.core.flattening import generate_flattening_lookup
+
+_logger = get_logger(__file__)
+def _setup_project(project_name: str) -> Project:
+    """
+    Setup function for the scripts project context
+
+    :param project_name: Name of the project to generate for
+    :return: `Project` instance representing the project context
+    """
+    project = Project(project_name)
+    _logger.info("Preparing packages")
+    project.package_manager.restore(inflate=True)
+    return project
+
+
+if __name__ == '__main__':
+    project = _setup_project("fdpg-ontology")
+    generate_flattening_lookup(project.package_manager)
+
